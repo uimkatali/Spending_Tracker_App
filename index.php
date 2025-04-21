eb875976d9b8b4e6c391656e7b7ad6907eab50b1
uimkatali <b8bf6b5@gmail.com>
2025-04-21 19:04:42 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" ></script>
+    <title>Document</title>
+</head>
+<body class="bg-cover bg-center bg-slate-600" >
+    <div class="flex justify-center justify-stretch p-8 bg-slate-300 full ">
+        <H2>hELLOOO</H2>
+    </div>
+</body>
+</html>
